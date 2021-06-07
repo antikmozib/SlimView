37de678dcecd6398adb32cfd9199b235b47a45ad
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-06-08 00:45:34 +1000

    Delete SimView.bat

--- SimView.bat
+++
@@ -1,3 +0,0 @@
-call mvn clean package
-REM start javaw -jar "target\simview-1.0.jar" "C:\Users\antik\Google Drive\BabyLove\40585864_1777075292390471_1358349352287862784_n.jpg"
-start javaw -jar "target\simview-1.0.jar" "D:\Nt Cld\InstaCache\katiemayo10_26863989_144450712906869_2568470604277612544_n.jpg"
