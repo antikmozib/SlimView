6b0d53ebd281df2925868bdd3cb0b4d5684e3966
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-01 23:53:12 +1000

    version change

--- jlink-run.bat
+++ jlink-run.bat
@@ -1,2 +1,2 @@
-REM start jlink-image\bin\javaw -jar "target\slimview-1.0.1.jar" %*
-start jlink-image\bin\javaw -jar "target\slimview-1.0.1.jar" %*
+REM start jlink-image\bin\javaw -jar "target\slimview-1.0.2.jar" %*
+start jlink-image\bin\javaw -jar "target\slimview-1.0.2.jar" %*
--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-REM start javaw -jar "target\slimview-1.0.1.jar" %*
-start javaw -jar "target\slimview-1.0.1.jar" %*
+REM start javaw -jar "target\slimview-1.0.2.jar" %*
+start javaw -jar "target\slimview-1.0.2.jar" %*
