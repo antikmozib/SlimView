854b7efcba994365434f9e739054d9313a2f258c
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-02 01:17:38 +1000

    update build dirs

--- jlink-build.bat
+++ jlink-build.bat
@@ -1,4 +1,5 @@
 call mvn clean package
-copy notice.txt target\notice.txt
-rmdir /s /q jlink-image
-jlink --module-path "%PATH_TO_FX_MODS%" --add-modules javafx.controls,javafx.graphics,java.base,java.desktop,javafx.fxml,javafx.base,javafx.swing,java.se,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output jlink-image
+mkdir target\win
+copy notice.txt target\win\notice.txt
+rmdir /s /q target\win\jlink-image
+jlink --module-path "%PATH_TO_FX_MODS%" --add-modules javafx.controls,javafx.graphics,java.base,java.desktop,javafx.fxml,javafx.base,javafx.swing,java.se,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\jlink-image
