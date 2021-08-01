4321810df3b8969164465128a021810859abe58b
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-02 01:54:49 +1000

    Update jlink-build.bat

--- jlink-build.bat
+++ jlink-build.bat
@@ -1,5 +1,24 @@
 call mvn clean package
+
+echo.
+echo Making directories
+echo.
 mkdir target\win
+
+echo.
+echo Copying notice
+echo.
 copy notice.txt target\win\notice.txt
+
+echo.
+echo Removing old jlink folder
+echo.
 rmdir /s /q target\win\jlink-image
+
+echo.
+echo Making new jlink image
+echo.
 jlink --module-path "%PATH_TO_FX_MODS%" --add-modules javafx.controls,javafx.graphics,java.base,java.desktop,javafx.fxml,javafx.base,javafx.swing,java.se,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\jlink-image
+
+echo.
+echo Done.
