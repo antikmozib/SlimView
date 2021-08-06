9b3a32289b7080067cfa4ed29c6209889b644f3f
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-07 08:16:41 +1000

    pom changes

--- jlink-build.bat
+++ jlink-build.bat
@@ -13,7 +13,7 @@ copy notice.txt target\win\notice.txt
 echo.
 echo Making new jlink image
 echo.
-jlink --module-path "%PATH_TO_FX_MODS%" --add-modules javafx.controls,javafx.graphics,java.base,java.desktop,javafx.fxml,javafx.base,javafx.swing,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\jlink-image
+jlink --module-path "%PATH_TO_FX_MODS%" --add-modules javafx.controls,javafx.graphics,java.base,javafx.fxml,javafx.base,javafx.swing,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\jlink-image
 
 echo.
 echo Done.
