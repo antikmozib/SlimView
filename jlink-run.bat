c86837f848ded20563f2171a07ca0604c7aaa04c
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-11 05:06:13 +1000

    version change

--- jlink-run.bat
+++ jlink-run.bat
@@ -1,6 +1,6 @@
 SET modules=javafx.controls,javafx.graphics,javafx.fxml,javafx.base,javafx.swing,imgscalr.lib,org.apache.commons.io,com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat.xml,metadata.extractor,org.apache.httpcomponents.httpclient,org.apache.httpcomponents.httpcore
 
-SET command=start target\win\runtime\bin\javaw --module-path target\modules --add-modules %modules% -jar target\slimview-1.0.4.jar %*
+SET command=start target\win\runtime\bin\javaw --module-path target\modules --add-modules %modules% -jar target\slimview-1.0.5.jar %*
 
 REM %command%
 start %command%
--- linux-jlink-run.sh
+++ linux-jlink-run.sh
@@ -1,2 +1,2 @@
 MODULES=javafx.controls,javafx.graphics,javafx.fxml,javafx.base,javafx.swing,imgscalr.lib,org.apache.commons.io,com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat.xml,metadata.extractor,org.apache.httpcomponents.httpclient,org.apache.httpcomponents.httpcore
-target/linux/runtime/bin/java --module-path target/linux/lib --add-modules $MODULES -jar target/slimview-1.0.4.jar
+target/linux/runtime/bin/java --module-path target/linux/lib --add-modules $MODULES -jar target/slimview-1.0.5.jar
