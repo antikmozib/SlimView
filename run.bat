aece57c2e16e65fb7db13dc1768dfa715a58fab9
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-11 09:21:10 +1000

    scripts cleanup

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
+@echo off
 SET modules=javafx.controls,javafx.graphics,javafx.fxml,javafx.base,javafx.swing,imgscalr.lib,org.apache.commons.io,com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat.xml,metadata.extractor,org.apache.httpcomponents.httpclient,org.apache.httpcomponents.httpcore
 
-SET command=start javaw --module-path target\modules --add-modules %modules% -jar target\slimview-1.0.5.jar %*
+SET param=javaw --module-path target\modules --add-modules %modules% -jar target\slimview-1.0.5.jar %*
 
 REM %command%
-start %command%
+start %param%
