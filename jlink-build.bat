861b94db395c2aa2373a06eb84f444f60b73ead3
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-11 01:30:27 +1000

    jigsaw fixes

--- jlink-build.bat
+++ jlink-build.bat
@@ -13,7 +13,7 @@ copy notice.txt target\win\notice.txt
 echo.
 echo Making new jlink image
 echo.
-jlink --module-path "%PATH_TO_FX_MODS%";target\modules --add-modules javafx.controls,javafx.graphics,javafx.fxml,javafx.base,javafx.swing,java.base,java.security.jgss,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\runtime
+jlink --module-path target\modules --add-modules java.base,java.security.jgss,javafx.controls,javafx.graphics,javafx.fxml,javafx.base,javafx.swing,jdk.net,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi --output target\win\runtime
 
 echo.
 echo Done.
