33f133678cd8c1f4b8e537885d8f88123014b068
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-08-26 03:24:44 +1000

    dependency updates
    org.apache.commons.commons-lang3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module io.mozib.slimview {
     requires javafx.base;
     requires javafx.swing;
     requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.httpcomponents.httpclient;
     requires com.fasterxml.jackson.core;
