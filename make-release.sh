a73eb97716ee565f636319564f8ecd8bdef2d215
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2021-09-06 02:46:30 +1000

    fixed untracked files

--- make-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./make-executable.sh
-
-if [ "$OSTYPE" == "cygwin" ]; then
-
-	printf "\nBuilding installer\n"
-	iscc "installer/slimview.iss"
-	
-fi
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MODULES=$(cat all-mods.txt | perl -p -e 's/\n/,/g/s/\s//g')
-
-# Excluded: com.twelvemonkeys.imageio.batik,xmlgraphics.commons
-
-./target/bin/runtime/bin/java --module-path target/bin/lib \
-	--add-modules $MODULES \
-	-jar target/slimview-1.0.7.jar $1
